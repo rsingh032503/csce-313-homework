06c11ea11c56b3d407fc9034825c1aad86738412
rxhxl-sxngh <rsingh032503@gmail.com>
2023-11-02 10:21:17 -0500

    completed q2

--- q2.cpp
+++ q2.cpp
@@ -3,33 +3,36 @@
 #include <unistd.h>
 #include <cstdlib>
 
-int your_function(int a, int b) {
-    return a + b;
+// function to call in order measure performance
+long long functionCall() {
+    return static_cast<long long>(rand()) + static_cast<long long>(rand());
 }
 
 int main() {
+    srand(time(NULL));
     const int numInvocations = 1000000;
     struct timespec start, end;
-    int result;
 
     // Measure the time for function calls
     clock_gettime(CLOCK_REALTIME, &start);
     for (int i = 0; i < numInvocations; ++i) {
-        result = your_function(rand(), rand()); // Sum of two random numbers
+        functionCall(); // Sum of two random numbers
     }
-    clock_gettime(CLOCK_REALTIME, &end);
-    double functionTime = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    clock_gettime(CLOCK_REALTIME, &end); 
+    // convert the nanoseconds from the end time stamp of tv_nsec to seconds and add them to the seconds from tv_sec
+    // this will give the complete time in seconds
+    double functionTime = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1e9); 
 
-    result = 0;
 
     // Measure the time for the getpid system call
     clock_gettime(CLOCK_REALTIME, &start);
     for (int i = 0; i < numInvocations; ++i) {
-        getpid(); // getpid is a simple system call
+        getpid();
     }
     clock_gettime(CLOCK_REALTIME, &end);
     double getpidTime = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
 
+    // divide by the number of invocations to get the average times for function calls and system calls and display them
     std::cout << "Average time for function call: " << functionTime / numInvocations << " seconds" << std::endl;
     std::cout << "Average time for getpid system call: " << getpidTime / numInvocations << " seconds" << std::endl;
 
