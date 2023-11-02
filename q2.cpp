72767402c60d90652e6b91a2facfe316316dab23
rxhxl-sxngh <rsingh032503@gmail.com>
2023-11-02 16:43:50 -0500

    q2 finished

--- q2.cpp
+++ q2.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
-#include <ctime>
-#include <unistd.h>
 #include <cstdlib>
+#include <unistd.h>
+#include <chrono>
 
-// function to call in order measure performance
+// function to call in order to measure performance
 long long functionCall() {
     return static_cast<long long>(rand()) + static_cast<long long>(rand());
 }
 
 int main() {
     srand(time(NULL));
-    const int numInvocations = 1000000;
-    struct timespec start, end;
+    const int numInvocations = 10000000;
 
     // Measure the time for function calls
-    clock_gettime(CLOCK_REALTIME, &start);
+    auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < numInvocations; ++i) {
         functionCall(); // Sum of two random numbers
     }
-    clock_gettime(CLOCK_REALTIME, &end); 
-    // convert the nanoseconds from the time elasped of tv_nsec to seconds and add them to the seconds from tv_sec time elapsed
-    // this will give the complete time in seconds
-    double functionTime = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1e9); 
-
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> functionTime = end - start;
 
     // Measure the time for the getpid system call
-    clock_gettime(CLOCK_REALTIME, &start);
+    start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < numInvocations; ++i) {
         getpid();
     }
-    clock_gettime(CLOCK_REALTIME, &end);
-    // convert the nanoseconds from the time elasped of tv_nsec to seconds and add them to the seconds from tv_sec time elapsed
-    // this will give the complete time in seconds
-    double getpidTime = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> getpidTime = end - start;
 
     // divide by the number of invocations to get the average times for function calls and system calls and display them
-    std::cout << "Average time for function call: " << functionTime / numInvocations << " seconds" << std::endl;
-    std::cout << "Average time for getpid system call: " << getpidTime / numInvocations << " seconds" << std::endl;
+    std::cout << "Average time for function call: " << functionTime.count() / numInvocations << " seconds" << std::endl;
+    std::cout << "Average time for getpid system call: " << getpidTime.count() / numInvocations << " seconds" << std::endl;
 
     return 0;
 }
+
